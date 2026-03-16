821a2ab258937887c5a3c2e59064e300dba8c558
Ahinsree B <ahinsree2000@gmail.com>
2026-03-16 13:22:21 +0530

    Add exercises_2 for Week 1: Introduce basic Python data types and their usage

---
+++ Semester-1/Week-1/Exercises/exercises_2_week1.py
@@ -0,0 +1,15 @@
+X1 = "Hello, World!"
+X2 = 42
+X3 = 3.14
+X4 = True
+X5 = None
+print(X1)
+print(type(X1))
+print(X2)
+print(type(X2))
+print(X3)
+print(type(X3))
+print(X4)
+print(type(X4))
+print(X5)
+print(type(X5))
