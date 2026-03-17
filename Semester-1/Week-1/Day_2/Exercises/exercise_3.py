25a60588ffdcab9a5ebefbace4525bd44e1d9bf8
Ahinsree B <ahinsree2000@gmail.com>
2026-03-17 17:20:13 +0530

    Add Exercise 3: Implement grading system based on score input

---
+++ Semester-1/Week-1/Day_2/Exercises/exercise_3.py
@@ -0,0 +1,11 @@
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("Grade: A.","Excellent!🏆")
+elif score >= 80:
+    print("Grade: B.","Good job!✅")
+elif score >= 70:
+    print("Grade: C.","Keep going!📘")
+elif score >= 60:
+    print("Grade: D.","You can do better!⚠️")
+else:
+    print("Grade: F.","Please revise and try again!❌")
