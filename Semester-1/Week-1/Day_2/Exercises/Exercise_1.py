f0069cbb337c17817bc50e82e94afe46aeeed05f
Ahinsree B <ahinsree2000@gmail.com>
2026-03-17 16:17:30 +0530

    Add Exercise 1: Implement comparison operations with examples

---
+++ Semester-1/Week-1/Day_2/Exercises/Exercise_1.py
@@ -0,0 +1,10 @@
+x = 50
+y = 30
+print(f"x == y: {x == y}")
+print(f"x != y: {x != y}")
+print(f"x > y: {x > y}")
+print(f"x < y: {x < y}")
+print(f"x >= y: {x >= y}")
+print(f"x <= y: {x <= y}")
+print(f"x is y: {x is y}")
+print(f"x is not y: {x is not y}")
