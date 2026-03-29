60ff88518983535221485aa13de7bb9cbdc34ca5
Ahinsree B <ahinsree2000@gmail.com>
2026-03-29 22:39:06 +0530

    Fix formatting in project.py for improved readability

--- Semester-1/Week-1/Day_4/Project/project.py
+++ Semester-1/Week-1/Day_4/Project/project.py
@@ -13,16 +13,16 @@ while attempts < max_attempts:
     guess = int(input(f"Attempt {attempts + 1}/{max_attempts} - your guess: "))
     attempts += 1
     if guess == secret_number:
-        print(f"🎉Congratulations! You've guessed the number {secret_number} in {attempts} attempts!")
+        print(f"🎉 Congratulations! You've guessed the number {secret_number} in {attempts} attempts!")
         break
     elif guess < secret_number:
-        print("📈Too Low! Try again.")
+        print("📈 Too Low! Try again.")
     else:
-        print("📉Too High! Try again.")
+        print("📉 Too High! Try again.")
     remaining_attempts = max_attempts - attempts
     if remaining_attempts > 0:
         print(f"You have {remaining_attempts} attempts Left.")
-    else:
-        print(f"❌ Out of attempts! The secret number was {secret_number}. Better luck next time!")
+else:
+    print(f"❌ Out of attempts! The secret number was {secret_number}. Better luck next time!")
     print("-" * 40)
         
