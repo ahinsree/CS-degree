e325ef15cb415bc45fffb5f6cb6f4e26c4ab3eee
Ahinsree B <ahinsree2000@gmail.com>
2026-03-29 16:24:18 +0530

    Add exercise_1.py: Implement tasks for number input and divisibility check

---
+++ Semester-1/Week-1/Day_4/Exercuses/exercise_1.py
@@ -0,0 +1,18 @@
+# Task 1: Keep asking for numbers
+# Stop when user enters a negative number
+# Print how many numbers entered before stopping
+count = 0
+while True:
+    num = int(input("Enter a number: "))
+    if num < 0:
+        print(f"your entered {count} number befor stopping")
+        break
+    count += 1 
+    
+# Task 2: Find first number 1–50
+# divisible by BOTH 6 and 8
+for i in range(1, 51):
+    if i % 6 == 0 and i % 8 == 0:
+        print(f"The first number between 1 and 50 that is divisible by both 6 and 8 is: {i}")
+        break
+        
